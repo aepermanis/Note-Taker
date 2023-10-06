fd9ac5ae9fa7402a6f13462af8d4f994819f3122
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-10-06 18:56:19 -0400

    updated server paths more to make sure that app loads correctly, fixed notes route to load notes properly

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -13,7 +13,7 @@ router.get('/notes', (req,res)=>{
 
 
 router.get('/api/notes', async (req,res)=>{
-        const notes = await JSON.parse(fs.readFileSync('../db/db.json'))
+        const notes = await JSON.parse(fs.readFileSync('Develop/db/db.json'))
         console.log(notes)
         res.json(notes)
 });
@@ -22,11 +22,13 @@ router.post('/api/notes', (req,res)=>{
     try{
         let newNote = {
             title: req.body.title,
-            text: req.body.text
+            text: req.body.text,
+            id: '' + Math.floor(Math.random() * 100)
         }
-        const notes = JSON.parse(fs.readFileSync('../db/db.json'));
+        const notes = JSON.parse(fs.readFileSync('Develop/db/db.json'));
         notes.push(newNote);
-        fs.writeFile('db.json', JSON.stringify(notes))
+        fs.writeFileSync('Develop/db/db.json', JSON.stringify(notes))
+        res.json(notes)
     }catch{
         console.log(err)
     }
